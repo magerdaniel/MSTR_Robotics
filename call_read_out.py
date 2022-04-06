670e86d35e241e96bb6f4d875931c5dd373d2631
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2022-04-06 14:37:28 +0200

    Add files via upload

--- call_read_out.py
+++ call_read_out.py
@@ -1,22 +1,50 @@
-import read_out_md
-import pandas
+import read_out_md as read
+import pandas as pd
 import openpyxl
-conf=md_analytics_conf
-read=read_out_md
-pd=pandas
+import md_compare
 
+run_type="check"
 base_url="https://mstr.dwapplications.com:8443/MicroStrategyLibrary/api/"
-#MicroStrategy Tutorial
-ldprj=['5B30C5AB11E9C111DD5D0080EFA5420F']
+
+env_project_l=[{"env":"prod","base_url":"https://mstr.dwapplications.com:8443/MicroStrategyLibrary/api/",
+                "projects":["A10C8E64CE4FD26ED37D93BB7A1B4928"]}]
+cube_project_id="5B30C5AB11E9C111DD5D0080EFA5420F"
 username='hacker_admin'
 password='h4cker@!'
-#information to create new cube
-fld_id='75028E138C4733B6C4DFE9964AE0F125'
-cbe_name='MD_ANALYTICS_CUBE1'
-updatePolicy="Add"
-#existing cube to simply add data
-mtdi_id="77F5EE183A4D298CB66F5587E3F9F9D1"
-#open a new connection (mstrio)
-conn=read.get_conn(base_url,username=username,password=password,project_id=ldprj)
-#call read out function
-read.runreadout(conn=conn,chkprj_l=ldprj,fld_id=fld_id,updatePolicy=updatePolicy,cube_name=cube_name,mtdi_id=mtdi_id)
+
+
+conn_cube_load = read.get_conn(base_url, username=username, password=password, project_id=cube_project_id)
+#daniels folder in tutorial
+folder_id = '75028E138C4733B6C4DFE9964AE0F125'
+
+#read out projects
+if run_type=="read":
+    cube_name = 'test_1'
+    updatePolicy="upsert"
+    mtdi_id = "E94516ED3C4E8F8BFB0DE297C0CC85C2"
+    #mtdi_id=None
+    #open a new connection (mstrio)
+    conn=read.get_conn(base_url, username=username, password=password, project_id=cube_project_id)
+    #call read out function
+    read.runreadout_loop(conn=conn,conn_cube_load=conn_cube_load, cube_project_id=cube_project_id, cube_name=cube_name, folder_id=folder_id,
+                         updatePolicy=updatePolicy,env_project_l=env_project_l,mtdi_id=mtdi_id)
+
+if run_type=="check":
+    comp=md_compare
+    update_policy="Replace"
+    read_mtdi_id = "E94516ED3C4E8F8BFB0DE297C0CC85C2"
+
+    diff_d=comp.get_cube_data(conn=conn_cube_load,project_id=cube_project_id,mtdi_id=read_mtdi_id)
+    cube_name = 'compare_diff'
+
+    write_cube_mtdi_id="52E6738D3E4F0C33B1B5FCA41039E731"
+    write_cube_mtdi_id=None
+    write_cube_mtdi_id=comp.upload_data(conn=conn_cube_load,
+                     project_id=cube_project_id,
+                     mtdi_id=write_cube_mtdi_id,
+                     tbl_upd_dict=diff_d,
+                     cube_name=cube_name,
+                     folder_id=folder_id,
+                     update_policy=update_policy)
+    print("cube " +write_cube_mtdi_id + " loaded")
+    pass
--- read_out_md.py
+++ read_out_md.py
@@ -1,76 +1,121 @@
-from mstrio import connection
+from mstrio.connection import Connection
+from mstrio.project_objects.datasets import SuperCube
 from datetime import datetime
 import cubeload
 import pandas as pd
 import openpyxl
 import json
+import numpy as np
 
 ##mtdi_structure_json = '{"name": "MD_ANALYTICS_CUBE","description": "My first time","folderId": "09988C274CEA09ABB1DE66B511521CCF","tables": [{"name": "all_mppd_tbl_col","columnHeaders": [{"name": "project_guid","dataType": "STRING"},{"name": "table_guid","dataType": "STRING"},{"name": "logical_table_name","dataType": "STRING"},{"name": "physical_table_name","dataType": "STRING"},{"name": "column_guid","dataType": "STRING"},{"name": "column_name","dataType": "STRING"}]}],"attributes": [{"name": "project_guid","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "project_guid"}]}]},{"name": "table_guid","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "table_guid"}]}]},{"name": "logical_table_name","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "logical_table_name"}]}]},{"name": "physical_table_name","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "physical_table_name"}]}]},{"name": "column_guid","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "column_guid"}]}]},{"name": "column_name","attributeForms": [{"category": "ID","expressions": [{"tableName": "all_mppd_tbl_col","columnName": "column_name"}]}]}]}'
 ##mtdi_upload_session_body ='{"tables": [{"name": "all_mppd_tbl_col","updatePolicy": "REPLACE","orientation": "ROW"}]}'
 #Neu
 columns = ["project_guid", "table_guid", "logical_table_name", "physical_table_name", "column_guid", "column_name"]
-def get_tbl_col(base_url,conn, prj_id, tbl_guid,load_ts):
+def get_tbl_col(base_url,conn, prj_id, tbl_guid,load_vars):
     inst_u = f"{base_url}/api/model/tables/{tbl_guid}"
     headers = {'X-MSTR-ProjectID': prj_id}
     cols = conn.get(inst_u)
     tblrow,prjtblcol = [],[]
-    for c in cols.json()["physicalTable"]["columns"]:
-        tblrow=[]
-        tblrow.append(load_ts)
-        tblrow.append(prj_id)
-        tblrow.append(cols.json()["information"]["objectId"])
-        tblrow.append(cols.json()["information"]["name"])
-        tblrow.append(cols.json()["physicalTable"]["information"]["name"])
-        tblrow.append(c["information"]["objectId"])
-        tblrow.append(c["information"]["name"])
-        prjtblcol.append(tblrow)
+
+    if "facts" in cols.json():
+        for f in cols.json()["facts"]:
+            tblrow=_add_tbl_col_(cols=cols,load_vars=load_vars,prj_id=prj_id)
+            #fact expression
+            tblrow.append(f["expression"]["text"])
+            #fact name
+            tblrow.append(f["information"]["name"])
+            #fact object ID
+            tblrow.append(f["information"]["objectId"])
+            #object type
+            tblrow.append(f["information"]["subType"])
+            prjtblcol.append(tblrow)
+
+    if "attributes" in cols.json():
+        for a in cols.json()["attributes"]:
+            tblrow=_add_tbl_col_(cols=cols,load_vars=load_vars,prj_id=prj_id)
+            #Attributeformexpression
+            tblrow.append(a["forms"][0]["expression"]["text"])
+            #Attribute Name
+            tblrow.append(a["information"]["name"])
+            #Attribute Name
+            tblrow.append(a["information"]["objectId"])
+            #Object type
+            tblrow.append(a["information"]["subType"])
+            prjtblcol.append(tblrow)
+
     return prjtblcol
+
+def _add_tbl_col_(cols=None,load_vars=None,prj_id=None):
+    tblrow = []
+    tblrow.extend(list(load_vars.values()))
+    tblrow.append(prj_id)
+    tblrow.append(cols.json()["information"]["objectId"])
+    tblrow.append(cols.json()["information"]["name"])
+    t_name=cols.json()["physicalTable"]["information"]["name"]
+    tblrow.append(cols.json()["physicalTable"]["information"]["name"])
+    return tblrow
 
 def get_prj_tbl(conn):
     inst_u = f"{conn.base_url}/api/model/tables"
     r = conn.get(inst_u)
     return r
 
-def runreadout(conn=None,chkprj_l=None,fld_id=None,updatePolicy="REPLACE",mtdi_id=None,cube_name=None):
+def runreadout(conn=None,conn_cube_load=None,cube_project_id=None,cube_name=None,folder_id=None,
+                    updatePolicy="REPLACE",mtdi_id=None,project=None,en_v="xxxxx"):
     cube_load = cubeload.load_cube(conn)
-    #define globals
-    load_ts= datetime.now()
-
     #define target table for data upload
-    tbl_name = "all_mppd_tbl_col1"
-
+    tbl_name = "all_mppd_tbl_col"
+    #
+    load_col_ord_l=["env","base_url","username","user_id","load_ts"]
+    #define globals
+    load_vars={}
+    load_vars["env"]=en_v
+    load_vars["base_url"]=conn.base_url
+    load_vars["username"]=conn.username
+    load_vars["user_id"]=conn.user_id
+    load_vars["load_ts"]= str(datetime.now().timestamp())
     #do the readout
     tbl=[]
-    for p in chkprj_l:
-        conn.project_id=p
-        r= get_prj_tbl(conn)
-        for t in r.json()["tables"]:
-            tbl.append({"prj":p,"tbl_guid":t["information"]["objectId"]})
+    conn.project_id=project
+    r= get_prj_tbl(conn)
+    load_col_ord_l.append("project_guid")
+    for t in r.json()["tables"]:
+        #print(t["information"]["name"])
+        tbl.append({"prj":project,"tbl_guid":t["information"]["objectId"]})
     tblcol=[]
     for t in tbl:
         #don't forget to add your globals
-        tblcol.extend(get_tbl_col(conn.base_url,conn,t["prj"],t["tbl_guid"],load_ts))
-    #finish read out
-
-    #create cubee if not proided
-    if not mtdi_id:
-        p_cbe_def = pd.read_excel("TBL_COL_CUBE.xlsx", sheet_name="MyCube")
-        #create cube
-        mtdi_id=cube_load.create_cube(p_cbe_def=p_cbe_def, cube_name=cube_name, fld_id=fld_id)
-        #the first upload must use Replace
-        cube_load.trigger_upload(conn=conn, mtdi_id=mtdi_id, tbl_name=tbl_name, tbl_data=tblcol,
-                                 updatePolicy="REPLACE")
+        #print(t["tbl_guid"])
+        tblcol.extend(get_tbl_col(conn.base_url,conn,t["prj"],t["tbl_guid"],load_vars))
+    #colums added by get_tbl_col
+    load_col_ord_l.extend(["table_guid","logical_table_name","physical_table_name","expression","object_name","object_guid","object_type"])
+    #np_tblcol=np.array(tblcol)
+    load_df = pd.DataFrame(tblcol, columns=load_col_ord_l)
+    if mtdi_id ==None:
+        ds = SuperCube(connection=conn_cube_load, name=cube_name)
+        ds.add_table(name=tbl_name, data_frame=load_df, update_policy="replace")
+        ds.create(folder_id=folder_id)
+        mtdi_id=ds.id
     else:
-        #upload the data to existing cube
-        cube_load.trigger_upload(conn=conn, mtdi_id=mtdi_id,
-                                 tbl_name=tbl_name,tbl_data=tblcol,
-                                 updatePolicy=updatePolicy)
+        ds = SuperCube(connection=conn_cube_load, id=mtdi_id)
+        ds.add_table(name=tbl_name, data_frame=load_df, update_policy=updatePolicy)
+        ds.update()
+        mtdi_id=ds.id
+    return ds.id
+
+def runreadout_loop(conn=None,conn_cube_load=None,cube_project_id=None,cube_name=None,folder_id=None,
+                    updatePolicy="REPLACE",mtdi_id=None,env_project_l=None):
+    for en_v in env_project_l:
+        for project in en_v["projects"]:
+           conn.headers["X-MSTR-ProjectID"]=project
+           mtdi_id=runreadout(conn=conn,conn_cube_load=conn_cube_load,mtdi_id=mtdi_id,cube_project_id=cube_project_id,folder_id=folder_id
+                           , cube_name=cube_name,en_v=en_v["env"],project=project,updatePolicy=updatePolicy )
+
 
 def get_conn(base_url=None, username=None,password=None,project_id=None):
-    conn= connection.Connection(base_url=base_url,username=username,
+    #in mstrio connenction the parameter ['Content-type'] is not set to "application/json"
+    #thus we need our own conn to eensure all functions of this
+    conn = Connection(base_url=base_url,username=username,
                                 password=password,project_id=project_id)
     conn.headers['Content-type']="application/json"
     return conn
-
-
-
