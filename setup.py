3f6d6f414c9cfea218d3db7684a42dbf5ee02f4f
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2023-01-19 13:45:27 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #[tool.poetry]
 description = "Demo Python scripts to automating MicroStrategy devOps processes"
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(name='MSTR_Robotics_magerdaniel',
       version= "0.0.38",
       description="MicroStrateg(P)ython",
