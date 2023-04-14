33a2baa4f10fa89d3489ed1fdcee8ddd9be10d99
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2023-04-14 14:34:05 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+#[tool.poetry]
+description = "Demo Python scripts to automating MicroStrategy devOps processes"
+
+from distutils.core import setup
+#from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(name='MSTR_Robotics_magerdaniel',
+      version= "0.1.1",
+      description="MicroStrateg(P)ython",
+      author="Daniel Mager",
+      author_email='"danielmager@gmx.de"',
+      url="https://github.com/magerdaniel",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['mstr_robotics'],
+
+)
+
