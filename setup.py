4f4598e6d7d61b441de11ceee2b09552ea1db57e
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2023-11-19 18:20:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-#[tool.poetry]
 description = "Demo Python scripts to automating MicroStrategy devOps processes"
+from distutils.core import setup
 
-#from distutils.core import setup
-from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
+
+for scheme in list(INSTALL_SCHEMES.values()):
+    scheme['data'] = scheme['purelib']
+
 long_description = (this_directory / "README.md").read_text()
 setup(name='MSTR_Robotics_magerdaniel',
-      version= "0.2.4",
-      description="MSTR_Robotics The Schema Monitor",
+      version= "0.2.5",
+      description="MicroStrateg(P)ython",
       author="Daniel Mager",
       author_email='"danielmager@gmx.de"',
       url="https://github.com/magerdaniel",
       long_description=long_description,
-      packages=find_packages(),
-      include_package_data=True,
       long_description_content_type='text/markdown',
-
-
+      packages=['mstr_robotics'],
+      data_files=[('notebooks', ['notebooks/jup_schema_monitor.ipynb'])],
 )
-
