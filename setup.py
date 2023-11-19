7ee430f685731d5047abf560129a4ecd9afa7a4c
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2023-11-19 12:03:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(name='MSTR_Robotics_magerdaniel',
-      version= "0.1.1",
-      description="MicroStrateg(P)ython",
+      version= "0.2.0",
+      description="MSTR_Robotics "The Schema Monitor",
       author="Daniel Mager",
       author_email='"danielmager@gmx.de"',
       url="https://github.com/magerdaniel",
