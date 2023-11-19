a0eb2c8a989f7fcbc67a71f8686d5395431ae8bf
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2023-11-19 17:47:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #[tool.poetry]
 description = "Demo Python scripts to automating MicroStrategy devOps processes"
 
-from distutils.core import setup
-#from setuptools import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
@@ -13,6 +13,8 @@ setup(name='MSTR_Robotics_magerdaniel',
       author_email='"danielmager@gmx.de"',
       url="https://github.com/magerdaniel",
       long_description=long_description,
+      packages=find_packages(),
+      include_package_data=True,
       long_description_content_type='text/markdown',
       packages=['mstr_robotics'],
 
