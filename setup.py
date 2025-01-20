b7e3154af1d79e445362460e1dd45f89d2d3cd42
Daniel Mager <75115555+magerdaniel@users.noreply.github.com>
2025-01-20 09:23:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-description = "Demo Python scripts to automating MicroStrategy devOps processes"
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 from pathlib import Path
+
+# Read the long description from README.md
 this_directory = Path(__file__).parent
-
-for scheme in list(INSTALL_SCHEMES.values()):
-    scheme['data'] = scheme['purelib']
-
 long_description = (this_directory / "README.md").read_text()
-setup(name='MSTR_Robotics_magerdaniel',
-      version= "0.3.03",
-      description="MicroStrateg(P)ython",
-      author="Daniel Mager",
-      author_email='"danielmager@gmx.de"',
-      url="https://github.com/magerdaniel",
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=['mstr_robotics'],
-      install_requires=[
-        "mstrio-py==11.4.6.101",  # MicroStrategy Python Library
-        "langchain",  # Framework for developing LLM-powered applications
-        "openai",  # OpenAI Python client
-        "flashtext",  # Library for keyword extraction and replacement
-        # itertools is a standard Python library and does not need to be installed
-        ],
-      package_data={'mstr_robotics': ['jup_schema_monitor.ipynb']},
+
+setup(
+    name="MSTR_Robotics_magerdaniel",
+    version="0.3.04",
+    description="MicroStrateg(P)ython",
+    author="Daniel Mager",
+    author_email="danielmager@gmx.de",
+    url="https://github.com/magerdaniel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[
+        "mstrio-py==11.4.6.101",  # Specify the exact or compatible version
+        "langchain",
+        "openai",
+        "flashtext>=2.7",  # Add a minimum version if applicable
+    ],
+    package_data={"mstr_robotics": ["jup_schema_monitor.ipynb"]},  # Include additional files
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",  # Specify the required Python version
 )
